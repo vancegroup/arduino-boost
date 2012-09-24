a07963adf3e4bf77eee1b30bbf101fd760c32fd8
Ryan Pavlik <rpavlik@iastate.edu>
2012-09-24 11:16:10 -0500

    Remove define workarounds from dummy header.

--- boost_1_51_0.h
+++ boost_1_51_0.h
@@ -1,7 +1,2 @@
 // Dummy include file for Arduino's sake.
 
-#define BOOST_NO_INTRINSIC_WCHAR_T
-#define BOOST_DISABLE_ASSERTS
-#define BOOST_NO_EXCEPTIONS
-#define BOOST_EXCEPTION_DISABLE
-#undef F
